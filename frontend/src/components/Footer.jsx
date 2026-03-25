6b5e29b0629e893c2f2820e8b81134f034cfbd0b
Saleh <saleh.662000@gmail.com>
2026-03-25 14:34:22 +0000

    refactor: remove logo imports and adjust footer layout

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,7 +1,5 @@
 // src/components/Footer.js
 import React from 'react';
-import euLogo from '../assets/eu_logo.png';
-import reconLogo from '../assets/recon4imd_logo.png';
 import '../styles/components/Footer.css';
 function Footer() {
   return (
@@ -15,10 +13,10 @@ function Footer() {
             <p className="footer-brand mb-0">OpenKineticsPredictor</p>
           </div>
 
-          {/* Column 2: Funding Information and Logos */}
+          {/* Column 2: Funding Information */}
           <div className="col-lg-8 col-md-12">
             <div className="d-flex justify-content-center justify-content-lg-end align-items-center">
-              <p className="funding-text me-4 mb-0">
+              <p className="funding-text mb-0">
                 EU Horizon Europe (#101080997)
                 &ensp;·&ensp;
                 Swiss SERI (#23.00232)
@@ -33,14 +31,6 @@ function Footer() {
                 &ensp;·&ensp;
                 EU Horizon 2020 (#686070 &amp; #814650)
               </p>
-              <div className="footer-logos">
-                <a href="https://www.recon4imd.org/" target="_blank" rel="noopener noreferrer">
-                  <img src={reconLogo} alt="Recon4IMD Logo" className="footer-logo" />
-                </a>
-                <a href="https://cordis.europa.eu/project/id/101080997" target="_blank" rel="noopener noreferrer">
-                  <img src={euLogo} alt="European Union Logo" className="footer-logo" />
-                </a>
-              </div>
             </div>
           </div>
 
