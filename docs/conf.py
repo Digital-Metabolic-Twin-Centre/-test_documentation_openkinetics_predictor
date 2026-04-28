62e1860cec4741188c5fa1a8276ca9d3ff02a98e
Chidi <chidiesobe@gmail.com>
2026-04-28 14:40:41 +0000

    chore: conf file update

--- docs/conf.py
+++ docs/conf.py
@@ -1,6 +1,6 @@
 from datetime import datetime
 
-project = "Student Project Documentation"
+project = "Open Kinetic Predictor"
 author = "Digital Metabolic Twin Centre"
 copyright = f"{datetime.now().year}, {author}"
 
